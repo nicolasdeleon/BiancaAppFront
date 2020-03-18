4fe0febb6ae1290de9839373a5ebffd798e36020
nico <ndeleon@itba.edu.ar>
2020-03-18 03:14:20 -0300

    toy quemado son 3am

--- components/EventDetail.js
+++ components/EventDetail.js
@@ -30,6 +30,8 @@ const EventDetail = props =>{
     const [doAnimation, setDoAnimation] = useState(false)
     const fadeFirstStep = useAnimation({doAnimation, duration: 500, easing: Easing.linear, callback: ()=>{}, delay: 0})
 
+    const [readyToExchangeColor, setReadyToExchangeColor] = useState("#D3D3D3")
+
     const fadeInSecondStep = useAnimation({doAnimation, duration: 700, easing: Easing.bounce, callback: ()=>{}, delay: 300})
 
 
@@ -57,7 +59,7 @@ const EventDetail = props =>{
                 { 
                 translateX:  fadeInSecondStep.interpolate({
                     inputRange: [0, 1],
-                    outputRange: [300, 0]
+                    outputRange: [500, 0]
                     })
                 }
         ],
@@ -65,13 +67,13 @@ const EventDetail = props =>{
 
     return (
         <View style={styles.screen}>
-                <Animated.View style={{...styles.circle,...animateFadeFirstStep}}>
+                <Animated.View style={{...styles.circle,...animateFadeFirstStep, backgroundColor: Colors.primary}}>
                     <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center',}} onPress={startPublishedAnimation}>
                         <Text style={styles.circleText}>Clickea acá para notificar haber subido la historia y obtener el beneficio</Text>
                     </TouchableOpacity>
                 </Animated.View>
-                <Animated.View style={{...styles.circle, ...animateFadeInSecondStep}}>
-                    <Text style={styles.circleText}>Clickea acá para notificar haber subido la historia y obtener el beneficio</Text>
+                <Animated.View style={{...styles.circle, ...animateFadeInSecondStep, backgroundColor:readyToExchangeColor}}>
+                    <Text style={styles.circleText}>Haz click para canjear tu beneficio!</Text>
                 </Animated.View>
         </View>
     )
@@ -91,7 +93,6 @@ const styles = StyleSheet.create({
         width: 250,
         height: 250,
         borderRadius: 250/2,
-        backgroundColor: Colors.primary,
         justifyContent:'center',
         alignItems:'center',
         position: 'absolute',
@@ -101,7 +102,20 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans-bold',
         fontSize: 18,
         textAlign: 'center'
-    }
+    },
+    overlay: {
+        width: 250,
+        height: 250,
+        borderRadius: 250/2,
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        opacity: 0.7,
+        backgroundColor: 'black',
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      } 
 });
 
 export default EventDetail
